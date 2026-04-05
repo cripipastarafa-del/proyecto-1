6232ac2965c38a2704c8ed30b73e13a9a592e514
cripipastarafa-del <cripipastarafa@gmail.com>
2026-04-05 18:00:36 +0200

    fix: close helmet middleware parentheses in server.js"

--- server.js
+++ server.js
@@ -11,18 +11,7 @@ const runner            = require('./test-runner');
  
 const app = express();
  
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.disable('strict-transport-security');
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff())
-app.use(helmet.ieNoOpen());
-var ninetyDaysInSeconds = 90*24*60*60;
-app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
- 
+
 app.use('/public', express.static(process.cwd() + '/public'));
  
 app.use(cors({origin: '*'})); //For FCC testing purposes only
